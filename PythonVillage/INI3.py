779f3f49e7b05477bb8a822f525d84baf9d76159
Caleb Hendren <109324308+CalebHendren@users.noreply.github.com>
2025-05-23 12:30:01 -0400

    Added INI2 and INI3

---
+++ PythonVillage/INI3.py
@@ -0,0 +1,6 @@
+string = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+a = 22
+b = 27
+c = 97
+d = 102
+print(string[a:b+1] + " " + string[c:d+1])
